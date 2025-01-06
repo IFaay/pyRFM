efa5690dc617a6fd25e6e57003c2f41ca9061ded
IFaay <20217907001@stu.suda.edu.cn>
2025-01-06 14:29:05 +0800

    Repair doc

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 # 将项目的根目录添加到 sys.path
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 project = 'pyRFM'
 copyright = '2025, Yifei Sun'
