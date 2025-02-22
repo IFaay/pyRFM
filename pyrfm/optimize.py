5c3da696ef3b729ad1ce99d92cc3fc1fd98d6c41
IFaay <20217907001@stu.suda.edu.cn>
2025-02-23 01:16:07 +0800

    refactor(optimize): replace GivensQR with BatchQR and optimize tensor operations
    Refactor the QR decomposition implementation by replacing GivensQR with BatchQR. This change simplifies the code and improves efficiency by leveraging PyTorch's built-in QR decomposition functions. Additionally, tensor operations have been optimized to reduce unnecessary computations and improve readability. The changes also include normalizing input rows before processing and updating the relative residual calculation for better accuracy.

--- pyrfm/optimize.py
+++ pyrfm/optimize.py
@@ -19,29 +19,44 @@ class GivensRotation:
         self.i = i
         self.k = k
         if b == 0.0:
-            self.c = 1.0
-            self.s = 0.0
+            self.c = torch.tensor(1.0)
+            self.s = torch.tensor(0.0)
         else:
             if abs(b) > abs(a):
                 tau = a / b
-                self.s = 1.0 / (1 + tau ** 2) ** 0.5
-                self.c = self.s * tau
+                self.s = torch.tensor(1.0 / (1 + tau ** 2) ** 0.5)
+                self.c = torch.tensor(self.s * tau)
             else:
                 tau = b / a
-                self.c = 1.0 / (1 + tau ** 2) ** 0.5
-                self.s = self.c * tau
+                self.c = torch.tensor(1.0 / (1 + tau ** 2) ** 0.5)
+                self.s = torch.tensor(self.c * tau)
 
-    def __matmul__(self, other):
+    def apply(self, other):
         if isinstance(other, torch.Tensor):
-            result = other.clone()
-            row_i = result[self.i]
-            row_k = result[self.k]
-            result[self.i] = self.c * row_i + self.s * row_k
-            result[self.k] = -self.s * row_i + self.c * row_k
-            return result
+            row_i = other[self.i].clone()
+            row_k = other[self.k].clone()
+            other[self.i] = row_i * self.c + row_k * self.s
+            other[self.k] = -row_i * self.s + row_k * self.c
+
+def givens(a, b):
+    a, b = float(a), float(b)
+    if b == 0.0:
+        c = 1.0
+        s = 0.0
+    else:
+        if abs(b) > abs(a):
+            tau = a / b
+            s = 1.0 / (1 + tau ** 2) ** 0.5
+            c = s * tau
+        else:
+            tau = b / a
+            c = 1.0 / (1 + tau ** 2) ** 0.5
+            s = c * tau
+    return torch.tensor(c), torch.tensor(s)
 
 
-class GivensQR:
+
+class BatchQR:
     def __init__(self, m, n_rhs):
         self.m = m  # number of cols
         self.n_rhs = n_rhs  # number of right hand sides
@@ -50,42 +65,82 @@ class GivensQR:
         self.solution = None
 
     def add_rows(self, A_new: torch.Tensor, b_new: torch.Tensor):
+        A_new_norm = torch.linalg.norm(A_new, ord=2, dim=1, keepdim=True)
+        A_new, b_new = A_new / A_new_norm, b_new / A_new_norm
+
+        # self.R = torch.cat([self.R, A_new], dim=0)
+        # self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
+        #
+        # if self.R.shape[0] >= self.m:
+        #     self.R, tau = torch.geqrf(self.R)
+        #     self.Qtb = torch.ormqr(self.R, tau, self.Qtb, transpose=True)[:self.m]
+        #     self.R = torch.triu(self.R)[:self.m]
+        #
+        #     self.solution = torch.linalg.solve_triangular(self.R, self.Qtb, upper=True)
+        #     logger.info("Relative Residual: {}".format(torch.norm(A_new @ self.solution - b_new)))
+        #
+        # else:
+        #     logger.warn("More conditions are needed to determine the solution.")
+        #     return
+
 
         if not self.R.shape[0] == self.m:
-            if self.R.shape[0] + A_new.shape[0] < self.m:
-                self.R = torch.cat([self.R, A_new], dim=0)
-                self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
-                logger.warn("More conditions are needed to determine the solution.")
+            self.R = torch.cat([self.R, A_new], dim=0)
+            self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
+
+            if self.R.shape[0] >=  self.m:
+                self.R, tau = torch.geqrf(self.R)
+                self.Qtb = torch.ormqr(self.R, tau, self.Qtb, transpose=True)[:self.m]
+                self.R = torch.triu(self.R)[:self.m]
+
+                self.solution = torch.linalg.solve_triangular(self.R, self.Qtb, upper=True)
+
+                logger.info("Relative Residual: {}".format(torch.norm(A_new @ self.solution - b_new) / torch.norm(b_new)))
                 return
 
-            elif self.R.shape[0] + A_new.shape[0] == self.m:
-                self.R = torch.cat([self.R, A_new], dim=0)
-                self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
-                self.R, tau = torch.geqrf(self.R)
-                self.Qtb = torch.ormqr(self.R, tau, self.Qtb, transpose=True)[:self.R.shape[1]]
-                self.R = torch.triu(self.R)
-                logger.warn("More conditions are needed to determine the solution.")
-                return
-            else:
-                self.R = torch.cat([self.R, A_new[:self.m - self.R.shape[0]]], dim=0)
-                self.Qtb = torch.cat([self.Qtb, b_new[:self.m - self.R.shape[0]]], dim=0)
-                self.R, tau = torch.geqrf(self.R)
-                self.Qtb = torch.ormqr(self.R, tau, self.Qtb, transpose=True)[:self.R.shape[1]]
-                self.R = torch.triu(self.R)
-                A_new = A_new[self.m:]
-                b_new = b_new[self.m:]
+            logger.warn("More conditions are needed to determine the solution.")
+            return
 
-        self.R = torch.cat([self.R, A_new], dim=0)
-        self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
+        else:
+            print(self.R.shape, A_new.shape)
 
-        for i in range(self.m, self.R.shape[0]):
-            for k in range(self.m):
-                givens = GivensRotation(self.R[k, k], self.R[i, k], k, i)
-                self.R = givens @ self.R
-                self.Qtb = givens @ self.Qtb
+            self.R = torch.cat([self.R, A_new], dim=0)
+            self.Qtb = torch.cat([self.Qtb, b_new], dim=0)
 
-        self.R = torch.triu(self.R[:self.m])
-        self.Qtb = self.Qtb[:self.m]
 
-        self.solution = torch.linalg.solve_triangular(self.R, self.Qtb, upper=True)
-        logger.info("Relative Residual: {}".format(torch.norm(A_new @ self.solution - b_new) / torch.norm(b_new)))
+            # for i in range(self.m, self.R.shape[0]):
+            #     # c = torch.zeros(self.m)
+            #     # s = torch.zeros(self.m)
+            #     # print(self.R[i][self.R[i] != 0])
+            #     for k in range(self.m):
+            #
+            #         if self.R[i, k] == 0:
+            #             continue
+            #         # c[k], s[k] = givens(self.R[k, k], self.R[i, k])
+            #
+            #
+            #         c, s = givens(self.R[k, k], self.R[i, k])
+            #         temp1 = c * self.R[k, k:] + s * self.R[i, k:]
+            #         self.R[i, k:] = -s * self.R[k, k:] + c * self.R[i, k:]
+            #         self.R[k, k:] = temp1
+            #
+            #         temp2 = c * self.Qtb[k] + s * self.Qtb[i]
+            #         self.Qtb[i] = -s * self.Qtb[k] + c * self.Qtb[i]
+            #         self.Qtb[k] = temp2
+            #         # givens = GivensRotation(self.R[k, k], self.R[i, k], k, i)
+            #         # givens.apply(self.R)
+            #         # givens.apply(self.Qtb)
+            #         # self.R = givens @ self.R
+            #         # self.Qtb = givens @ self.Qtb
+            #     # print(self.R[i][self.R[i] != 0])
+
+            self.R, tau = torch.geqrf(self.R)
+            self.Qtb = torch.ormqr(self.R, tau, self.Qtb, transpose=True)[:self.m]
+            self.R = torch.triu(self.R)[:self.m]
+
+            # self.R = torch.triu(self.R[:self.m])
+            # self.Qtb = self.Qtb[:self.m]
+
+            self.solution = torch.linalg.solve_triangular(self.R, self.Qtb, upper=True)
+
+            logger.info("Relative Residual: {}".format(torch.norm(A_new @ self.solution - b_new) / torch.norm(b_new)))
